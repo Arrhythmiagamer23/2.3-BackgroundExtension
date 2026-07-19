6239585633233650354a869ce32cfd33463d85b1
KDSealock <158773839+Arrhythmiagamer23@users.noreply.github.com>
2026-07-19 16:57:04 -0400

    Enhance GameManager with async loading methods
    Added new methods to GameManager for loading textures and fonts asynchronously. Modified the init function to include new functionality.

--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,109 @@
-/**
- * Include the Geode headers.
- */
 #include <Geode/Geode.hpp>
 
-/**
- * Brings cocos2d and all Geode namespaces to the current scope.
- */
 using namespace geode::prelude;
 
-/**
- * `$modify` lets you extend and modify GD's classes.
- * To hook a function in Geode, simply $modify the class
- * and write a new function definition with the signature of
- * the function you want to hook.
- *
- * Here we use the overloaded `$modify` macro to set our own class name,
- * so that we can use it for button callbacks.
- *
- * Notice the header being included, you *must* include the header for
- * the class you are modifying, or you will get a compile error.
- *
- * Another way you could do this is like this:
- *
- * struct MyMenuLayer : Modify<MyMenuLayer, MenuLayer> {};
- */
-#include <Geode/modify/MenuLayer.hpp>
-class $modify(MyMenuLayer, MenuLayer) {
-	/**
-	 * Typically classes in GD are initialized using the `init` function, (though not always!),
-	 * so here we use it to add our own button to the bottom menu.
-	 *
-	 * Note that for all hooks, your signature has to *match exactly*,
-	 * `void init()` would not place a hook!
-	*/
+#include <Geode/modify/GameManager.hpp>
+class $modify(MyGameManager, GameManager) {
+
 	bool init() {
-		/**
-		 * We call the original init function so that the
-		 * original class is properly initialized.
-		 */
-		if (!MenuLayer::init()) {
+
+		if (!GameManager::init()) {
 			return false;
 		}
 
-		/**
-		 * You can use methods from the `geode::log` namespace to log messages to the console,
-		 * being useful for debugging and such. See this page for more info about logging:
-		 * https://docs.geode-sdk.org/tutorials/logging
-		*/
-		log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
+    	const char* GameManager::getBGTexture(int index) {
+        	index = std::clamp(index, 0, 191);
+        	this->loadBackground(index);
+        	return cocos2d::CCString::createWithFormat("game_bg_%02d_001.png", index)->getCString();
+    	}
 
-		/**
-		 * See this page for more info about buttons
-		 * https://docs.geode-sdk.org/tutorials/buttons
-		*/
-		auto myButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
-			this,
-			/**
-			 * Here we use the name we set earlier for our modify class.
-			*/
-			menu_selector(MyMenuLayer::onMyButton)
-		);
+    	const char* GameManager::getFontFile(int index) {
+        	index = std::clamp(index, 0, 72);
+        	this->loadFont(index);
+        	if (index != 0) {
+            	return cocos2d::CCString::createWithFormat("gjFont%02d.fnt", index)->getCString();
+        	}
+       		return "bigFont.fnt";
+    	}
 
-		/**
-		 * Here we access the `bottom-menu` node by its ID, and add our button to it.
-		 * Node IDs are a Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/nodetree
-		*/
-		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(myButton);
+    	const char* GameManager::getFontTexture(int index) {
+        	index = std::clamp(index, 0, 72);
+        	this->loadFont(index);
+        	if (index != 0) {
+            	return cocos2d::CCString::createWithFormat("gjFont%02d.png", index)->getCString();
+        	}
+        	return "bigFont.png";
+    	}
 
-		/**
-		 * The `_spr` string literal operator just prefixes the string with
-		 * your mod id followed by a slash. This is good practice for setting your own node ids.
-		*/
-		myButton->setID("my-button"_spr);
+    	const char* GameManager::getGTexture(int index) {
+        	index = std::clamp(index, 0, 41);
+        	this->loadGround(index);
+        	return cocos2d::CCString::createWithFormat("groundSquare_%02d_001.png", index)->getCString();
+    	}
 
-		/**
-		 * We update the layout of the menu to ensure that our button is properly placed.
-		 * This is yet another Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/layouts
-		*/
-		menu->updateLayout();
+    	const char* GameManager::getMGTexture(int index) {
+        	index = std::clamp(index, 0, 6);
+        	this->loadMiddleground(index);
+        	return cocos2d::CCString::createWithFormat("fg_%02d_001.png", index)->getCString();
+    	}
 
-		/**
-		 * We return `true` to indicate that the class was properly initialized.
-		 */
-		return true;
-	}
+    	void GameManager::loadBackgroundAsync(int index) {
+        	index = std::clamp(index, 0, 191);
+        	if (m_loadingBG || m_loadedBgID == index) return;
+        	m_loadingBG = true;
+        	cocos2d::CCTextureCache::sharedTextureCache()->addImageAsync(
+            	cocos2d::CCString::createWithFormat("game_bg_%02d_001.png", index)->getCString(),
+            	this,
+            	callfuncO_selector(GameManager::finishedLoadingBGAsync),
+            	index,
+            	cocos2d::kCCTexture2DPixelFormat_RGBA8888
+        	);
+    	}
 
-	/**
-	 * This is the callback function for the button we created earlier.
-	 * The signature for button callbacks must always be the same,
-	 * return type `void` and taking a `CCObject*`.
-	*/
-	void onMyButton(CCObject*) {
-		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
+    	void GameManager::loadGroundAsync(int index) {
+        	index = std::clamp(index, 0, 41);
+        	if (m_loadingG || m_loadedGroundID == index) return;
+        	m_loadingG = true;
+        	auto hasSecondary = this->groundHasSecondaryColor(index);
+        	m_finishedLoadingG1 = false;
+        	m_finishedLoadingG2 = !hasSecondary;
+        	cocos2d::CCTextureCache::sharedTextureCache()->addImageAsync(
+            	cocos2d::CCString::createWithFormat("groundSquare_%02d_001.png", index)->getCString(),
+            	this,
+            	callfuncO_selector(GameManager::finishedLoadingGAsync1),
+            	index,
+            	cocos2d::kCCTexture2DPixelFormat_RGBA8888
+        	);
+        	if (!hasSecondary) return;
+        	cocos2d::CCTextureCache::sharedTextureCache()->addImageAsync(
+            	cocos2d::CCString::createWithFormat("groundSquare_%02d_2_001.png", index)->getCString(),
+            	this,
+            	callfuncO_selector(GameManager::finishedLoadingGAsync2),
+            	index,
+            	cocos2d::kCCTexture2DPixelFormat_RGBA8888
+        	);
+    	}
+
+    	void GameManager::loadMiddlegroundAsync(int index) {
+        	index = std::clamp(index, 0, 6);
+        	if (m_loadingG1 || m_loadedMG == index) return;
+        	m_loadingG1 = true;
+        	m_finishedLoadingMG1 = false;
+        	m_finishedLoadingMG2 = false;
+        	cocos2d::CCTextureCache::sharedTextureCache()->addImageAsync(
+            	cocos2d::CCString::createWithFormat("fg_%02d_001.png", index)->getCString(),
+            	this,
+            	callfuncO_selector(GameManager::finishedLoadingMGAsync1),
+            	index,
+            	cocos2d::kCCTexture2DPixelFormat_RGBA8888
+        	);
+        	cocos2d::CCTextureCache::sharedTextureCache()->addImageAsync(
+            	cocos2d::CCString::createWithFormat("fg_%02d_2_001.png", index)->getCString(),
+            	this,
+            	callfuncO_selector(GameManager::finishedLoadingMGAsync2),
+            	index,
+            	cocos2d::kCCTexture2DPixelFormat_RGBA8888
+        	);
+    	}
 	}
 };
